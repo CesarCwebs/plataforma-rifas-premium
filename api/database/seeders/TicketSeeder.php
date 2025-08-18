0ba6f534f5f180cfa438a0fee9aa0acb0b7568de
Cesar Bianchezi Perez <cesarbianchezi@gmail.com>
2025-08-18 09:29:28 +0000

    ADD: Crea el TicketSeeder y lo guarda en el repositorio

--- api/database/seeders/TicketSeeder.php
+++ api/database/seeders/TicketSeeder.php
@@ -4,19 +4,13 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Ticket; // Asegúrate de que esta línea esté
+use App\Models\Ticket;
 
 class TicketSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        // Asumimos que la rifa principal tiene el ID 1
         $raffleId = 1;
-
-        // Generamos 200 boletos para la rifa
         for ($i = 1; $i <= 200; $i++) {
             Ticket::create([
                 'number' => $i,
